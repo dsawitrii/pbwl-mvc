3b78202cd432ee67412162825ec6179f7fdbd3a8
dsawitrii <118437040+dsawitrii@users.noreply.github.com>
2023-05-29 22:04:26 +0700

    second

---
+++ app/views/home/index.php
@@ -0,0 +1,4 @@
+<h2>Selamat Datang Di Website E-Commerce</h2>
+
+<h3>Website ini merupakan online shop yang menjual berbagai jenis pakaian muslim, jilbab, mukena dan piyama khusus untuk wanita.
+    Didirikan sejak tahun 2020 dengan nama Diah Shop.</h3>
